7e85535a66ed37d361beffd86d621917575f9733
Giles Cope <gilescope@gmail.com>
2023-05-16 11:30:19 +0100

    make patchy fixup the Cargo.toml directly

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # patchy
 patch substrate / polkadot
 
+alternatives: diener - That's propper because it uses cargo metadata but doesn't work if cargo metadata doesn't work.
+
+It will alter the Cargo.toml in the current dir.
+
+It assumes that you have a dir structure:
+
+parent dir /
+    substrate /
+    polkadot /
+    cumulus /
+
+you run `patchy` in the cumulus and polkadot dirs to patch those branches.
+
 ## licensing
 unlicense
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
-use {regex::Regex, std::path::Path, walkdir::WalkDir};
+use {
+    regex::Regex,
+    std::path::Path,
+    std::{fs::File, fs::OpenOptions, io::Write},
+    walkdir::WalkDir,
+};
+
 const TGT: &str = "target";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let re = Regex::new(r#"name\s*=\s*"(.+?)""#)?;
     let is_target = |e: &Path| e.ancestors().any(|f| f.file_name() == Some(TGT.as_ref()));
 
-    let process = |project: &str| -> Result<(), Box<dyn std::error::Error>> {
-        println!(r#"[patch."https://github.com/paritytech/{project}"]"#);
+    let process = |project: &str| -> Result<String, Box<dyn std::error::Error>> {
+        let mut out = String::new();
+        out.push_str("\n");
+        out.push_str(&format!(
+            r#"[patch."https://github.com/paritytech/{project}"]"#
+        ));
         for entry in WalkDir::new(format!("../{project}"))
             .into_iter()
             .filter_map(Result::ok)
@@ -17,15 +27,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 let contents = std::fs::read_to_string(entry.path())?;
                 let name = &re.captures(&contents).unwrap()[1];
                 if name != "erasure_coding_fuzzer" {
-                    println!("{name} = {{ path = \"{}\" }}", target.display());
+                    out.push_str(&format!("\n{name} = {{ path = \"{}\" }}", target.display()));
                 }
             }
         }
-        Ok(())
+        Ok(out)
     };
-    if std::env::current_dir().unwrap().file_name() == Some("cumulus".as_ref()) {
-        process("polkadot")?;
-        println!();
-    }
-    process("substrate")
+    let mut out = if std::env::current_dir().unwrap().file_name() == Some("cumulus".as_ref()) {
+        process("polkadot")?
+    } else {
+        String::new()
+    };
+    out.push_str("\n");
+    out.push_str(&process("substrate")?);
+
+    println!("{}", &out);
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("./Cargo.toml")
+        .expect("Failed to open file");
+    file.write_all(out.as_bytes()).unwrap();
+
+    Ok(())
 }
